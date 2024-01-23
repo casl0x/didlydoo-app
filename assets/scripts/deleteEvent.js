4d904ad3bc35c81425705886ce002faa01857207
Denis Mathieu <denismathieu1203@gmail.com>
2024-01-23 12:22:28 +0100

    test

--- assets/scripts/deleteEvent.js
+++
@@ -1,9 +0,0 @@
-export function deleteEvent(eventId, objectsArray, arrayName, getStoredArray, storeArray){
-    let i = 0;
-    while (objectsArray[i].uniqueId != eventId){
-        i++;
-    }
-    objectsArray.splice(i, 1);
-    storeArray(objectsArray, arrayName);
-    getStoredArray(arrayName);
-}
