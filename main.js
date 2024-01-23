f9c021683a128143b859d98594be8ad6f886ae64
Denis Mathieu <denismathieu1203@gmail.com>
2024-01-23 16:44:10 +0100

    added dataBase functions

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
+import { postInfos, editEvent, deleteEvent, addDates, attendEvent, editAttendance } from "./assets/scripts/dataBaseFunctions.js";
+/*
+function test(){
+    postInfos("Test", ['2022-03-12', '2022-03-13', '2022-03-14', '2022-03-15'], "Denis", "Ceci est un test");
+}
+*/
 
+function test(){
+    
+}
+
+
+
+function edit(){
+    editEvent("378ee41aa49b", "Toust", "Dany", "New description");
+}
+
+let button = document.querySelector(".test");
+button.addEventListener('click', test);
+
+let editButton = document.querySelector(".edit");
+editButton.addEventListener('click', edit);
