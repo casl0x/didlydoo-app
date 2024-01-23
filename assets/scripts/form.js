f62f3f565c6e4641f50d40bfda1d891a08846434
Cassidy <cassidy.rouelle@gmail.com>
2024-01-23 11:40:47 +0100

    Cass → form blur bg

--- assets/scripts/form.js
+++ assets/scripts/form.js
@@ -1,10 +1,12 @@
 
 function addEvent (){
+    const container = document.querySelector('.container');
     const addBtn = document.querySelector('.form');
     const formEvent = document.querySelector('.event');
 
     addBtn.addEventListener('click', () => {
         formEvent.classList.toggle('open');
+        container.classList.toggle('blur');
     })
 }
 
@@ -12,7 +14,7 @@ function closeForm (){
     const validateBtn = document.querySelector('.event-button');
 
     validateBtn.addEventListener('click', () => {
-        formEvent.classList.toggle('open')
+        formEvent.classList.toggle('open');
     })
 }
 
