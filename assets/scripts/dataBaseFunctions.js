be24d7b03e1b8af9c9f94e60498d24fec2483ca8
Denis Mathieu <denismathieu1203@gmail.com>
2024-01-24 12:21:16 +0100

    added all database functions

--- assets/scripts/dataBaseFunctions.js
+++ assets/scripts/dataBaseFunctions.js
@@ -1,3 +1,48 @@
+export async function getAllEvents(){
+    try {
+        const response = await fetch('http://localhost:3000/api/events/');
+        const objectsArray = await response.json();
+
+        return objectsArray;
+    } catch(error){
+        console.log(error);
+    }
+}
+
+export async function getEvent (eventId){
+
+    try{
+        const response = await fetch('http://localhost:3000/api/events/' + eventId);
+        const object = await response.json();
+
+        return object;
+    } catch(error){
+        console.log(error);
+    }
+}
+
+export async function getAllAttendees(){
+    try {
+        const response = await fetch('http://localhost:3000/api/attendees/');
+        const objectsArray = await response.json();
+
+        return objectsArray;
+    } catch(error){
+        console.log(error);
+    }
+}
+
+export async function getAttendee(name){
+    try{
+        const response = await fetch('http://localhost:3000/api/attendees/' + name);
+        const object = await response.json();
+
+        return object;
+    } catch(error){
+        console.log(error);
+    }
+}
+
 export function postInfos(eventName, datesArray, eventAuthor, eventDescription){
     fetch('http://localhost:3000/api/events/', {
         method: 'POST',
@@ -16,7 +61,6 @@ export function postInfos(eventName, datesArray, eventAuthor, eventDescription){
         .catch(error => console.log(error));
 }
 
-
 export function editEvent (eventId, eventName, eventAuthor, eventDescription){
     fetch('http://localhost:3000/api/events/' + eventId, {
         method : 'PATCH',
@@ -33,7 +77,6 @@ export function editEvent (eventId, eventName, eventAuthor, eventDescription){
         .catch(error => console.log(error));
 }
 
-
 export function deleteEvent (eventId){
     fetch('http://localhost:3000/api/events/' + eventId, {
         method: 'DELETE',
@@ -45,7 +88,6 @@ export function deleteEvent (eventId){
         .then(data => {console.log(data)})
         .catch(error => console.log(error));
 }
-
 
 export function addDates (eventId, datesArray){
     fetch('http://localhost:3000/api/events/' + eventId + '/add_dates', {
@@ -62,7 +104,6 @@ export function addDates (eventId, datesArray){
         .catch(error => console.log(error));
 }
 
-
 export function attendEvent (eventId, attendeeName, datesAvailableArray){
     fetch('http://localhost:3000/api/events/' + eventId + '/attend', {
         method: 'POST',
@@ -78,7 +119,6 @@ export function attendEvent (eventId, attendeeName, datesAvailableArray){
         .then(data => {console.log(data)})
         .catch(error => console.log(error));
 }
-
 
 export function editAttendance (eventId, attendeeName, datesAvailableArray){
     fetch('http://localhost:3000/api/events/' + eventId + '/attend', {
--- main.js
+++ main.js
@@ -1,15 +1,15 @@
-import { postInfos, editEvent, deleteEvent, addDates, attendEvent, editAttendance } from "./assets/scripts/dataBaseFunctions.js";
+import { postInfos, editEvent, deleteEvent, addDates, attendEvent, editAttendance, getEvent, getAllEvents, getAllAttendees, getAttendee } from "./assets/scripts/dataBaseFunctions.js";
 /*
 function test(){
     postInfos("Test", ['2022-03-12', '2022-03-13', '2022-03-14', '2022-03-15'], "Denis", "Ceci est un test");
 }
 */
 
-function test(){
-    
+async function test(){
+        let data = await getAttendee("Michou");
+        console.log(data);
+
 }
-
-
 
 function edit(){
     editEvent("378ee41aa49b", "Toust", "Dany", "New description");
