9f6e805e5402853c16cc18ab818d1be8da694d8b
Cassidy <cassidy.rouelle@gmail.com>
2024-01-25 16:52:34 +0100

    Cass → display attendees

--- assets/scripts/modules/displayEvent.js
+++ assets/scripts/modules/displayEvent.js
@@ -1,4 +1,4 @@
-import { getAllEvents } from "./dataBaseFunction.js";
+import { getAllEvents, getAllAttendees } from "./dataBaseFunction.js";
 import { createEvent } from "./createevent.js";
 
 export async function displayEvent(){
@@ -13,5 +13,15 @@ export async function displayEvent(){
             createEvent(element);
         });
     }
+
+    let attendees = await getAllAttendees();
+    const attendDiv = document.createElement('div');
+    document.querySelector('.event').appendChild(attendDiv)
+    if (!attendees){
+        attendDiv.innerHTML = 'No attendees ! '
+    } else {
         
+    }
 }
+
+
